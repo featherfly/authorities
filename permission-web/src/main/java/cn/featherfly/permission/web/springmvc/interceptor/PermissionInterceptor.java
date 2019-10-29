bf43792287956ae8cc5d07f43c45d76fa012958c
zhongji <zhongj@cdmhzx.com>
2019-10-29 15:14:05 +0800

    升级依赖

--- permission-web/src/main/java/cn/featherfly/permission/web/springmvc/interceptor/PermissionInterceptor.java
+++ permission-web/src/main/java/cn/featherfly/permission/web/springmvc/interceptor/PermissionInterceptor.java
@@ -27,7 +27,7 @@ import cn.featherfly.web.spring.servlet.view.Result;
 
 /**
  * <p>
- * AuthorizedInterceptor
+ * PermissionInterceptor
  * </p>
  *
  * @author 钟冀
@@ -63,9 +63,7 @@ public class PermissionInterceptor implements HandlerInterceptor {
     }
 
     /**
-     * 在业务处理器处理请求之前被调用 如果返回false 从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链
-     * 如果返回true 执行下一个拦截器,直到所有的拦截器都执行完毕 再执行被拦截的Controller 然后进入拦截器链,
-     * 从最后一个拦截器往回执行所有的postHandle() 接着再从最后一个拦截器往回执行所有的afterCompletion()
+     * {@inheritDoc}
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -116,14 +114,16 @@ public class PermissionInterceptor implements HandlerInterceptor {
     }
 
     // 在业务处理器处理请求执行完成后,生成视图之前执行的动作
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
     }
 
     /**
-     * 在DispatcherServlet完全处理完请求后被调用
-     * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()
+     * {@inheritDoc}
      */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
