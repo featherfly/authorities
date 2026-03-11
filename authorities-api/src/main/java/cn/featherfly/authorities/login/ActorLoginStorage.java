2a4cf038f383ced144811fcd5a7396941626cb53
zhongj <featherfly@foxmail.com>
2026-03-11 18:21:40 +0800

    doc: fix javadoc

--- authorities-api/src/main/java/cn/featherfly/authorities/login/ActorLoginStorage.java
+++ authorities-api/src/main/java/cn/featherfly/authorities/login/ActorLoginStorage.java
@@ -14,22 +14,18 @@ import cn.featherfly.authorities.Actor;
  * @author zhongj
  */
 public interface ActorLoginStorage<I extends LoginInfo<A>, A extends Actor> {
+
     /**
-     * <p>
-     * 存储
-     * </p>
+     * 存储.
      *
-     * @param key
-     *        唯一标示
-     * @param actor
-     *        行动者
+     * @param key 唯一标示
+     * @param actor 行动者
+     * @return the LoginInfo
      */
     I store(String key, A actor);
 
     /**
-     * <p>
-     * 移除
-     * </p>
+     * 移除.
      *
      * @param actor
      *        行动者
@@ -37,9 +33,7 @@ public interface ActorLoginStorage<I extends LoginInfo<A>, A extends Actor> {
     void remove(A actor);
 
     /**
-     * <p>
-     * 是否包含指定key
-     * </p>
+     * 是否包含指定key.
      *
      * @param key
      *        key
@@ -48,9 +42,7 @@ public interface ActorLoginStorage<I extends LoginInfo<A>, A extends Actor> {
     boolean containsKey(String key);
 
     /**
-     * <p>
-     * 移除
-     * </p>
+     * 移除.
      *
      * @param key
      *        唯一标示
@@ -58,9 +50,7 @@ public interface ActorLoginStorage<I extends LoginInfo<A>, A extends Actor> {
     void remove(String key);
 
     /**
-     * <p>
      * 获取指定行动者的登录信息.
-     * </p>
      *
      * @param key
      *        唯一标示
@@ -69,9 +59,7 @@ public interface ActorLoginStorage<I extends LoginInfo<A>, A extends Actor> {
     I getLoginInfo(String key);
 
     /**
-     * <p>
      * 获取指定行动者的登录信息.
-     * </p>
      *
      * @param actor
      *        行动者
@@ -80,18 +68,14 @@ public interface ActorLoginStorage<I extends LoginInfo<A>, A extends Actor> {
     I getLoginInfo(A actor);
 
     /**
-     * <p>
      * 获取登录的行动者.
-     * </p>
      *
      * @return 登录的行动者
      */
     List<A> getLoginActors();
 
     /**
-     * <p>
      * 获取登录的行动者.
-     * </p>
      *
      * @return 登录的行动者
      */
